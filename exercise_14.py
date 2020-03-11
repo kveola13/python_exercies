2f4c361ef3e9ad8ec21099de43894c2e2970a0e3
olakv <kveola13@student.westerdals.no>
2020-03-11 21:38:29 +0100

    finished exercise_13

--- exercise_14.py
+++ exercise_14.py
@@ -4,9 +4,18 @@ def return_list_as_set(return_list):
     return new_list
 
 
+def return_list(list_returning):
+    new_list = []
+    for el in list_returning:
+        if el not in new_list:
+            new_list.append(el)
+    return new_list
+
+
 def main():
-    names = [1, 2, 3, 12, 4, 1, 2, 4, 5, 6, 71, 1, 1, 14, 4, 5, 4, 7, 8]
-    print(return_list_as_set(names))
+    demo_list = [1, 2, 3, 12, 4, 1, 2, 4, 5, 6, 71, 1, 1, 14, 4, 5, 4, 7, 8]
+    print(return_list_as_set(demo_list))
+    print(return_list(demo_list))
 
 
 if __name__ == '__main__':

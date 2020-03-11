e2f6057dc90dbb66a1e9f9ea98c289f7fab895d7
olakv <kveola13@student.westerdals.no>
2020-03-11 23:44:35 +0100

    supplements to exercise_16, added exercise_17

--- exercise_17.py
+++ exercise_17.py
@@ -1,7 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def request_article_titles(url):
+    request = requests.get(url)
+    request_html = request.text
+    article_finder = BeautifulSoup(request_html)
+    title = article_finder.find('span', 'articletitle')
+    return title
 
 
 def main():
-    return
+    request_article_titles('https://www.nytimes.com/')
 
 
 if __name__ == '__main__':

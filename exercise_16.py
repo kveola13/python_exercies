ee92ea7a9c1509911f0414af4e201d9046a32564
olakv <kveola13@student.westerdals.no>
2020-03-11 23:37:43 +0100

    supplements to exercise_16, added exercise_17

--- exercise_16.py
+++ exercise_16.py
@@ -1,14 +1,24 @@
 import random
 import string
 
+from random_word import RandomWords
+
 
 def password_generator(length_of_password):
-    return ''.join(
-        [random.choice(string.ascii_letters + string.digits + string.punctuation) for s in range(length_of_password)])
+    random_word = RandomWords()
+    random_digit_list = []
+    for num in range(length_of_password):
+        random_digit_list.append(random.choice(string.digits))
+    if length_of_password <= 4:
+        return random_word.get_random_word() + random_word.get_random_word() + ''.join(random_digit_list)
+    else:
+        return ''.join(
+            [random.choice(string.ascii_letters + string.digits + string.punctuation) for s in
+             range(length_of_password)])
 
 
 def main():
-    print(password_generator(12))
+    print(password_generator(5))
 
 
 if __name__ == '__main__':

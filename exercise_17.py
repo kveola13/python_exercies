c2dc37992e1cef2f0c6c17d4db21438e082a8714
olakv <kveola13@student.westerdals.no>
2020-03-12 21:16:04 +0100

    completed exercise_17

--- exercise_17.py
+++ exercise_17.py
@@ -5,9 +5,14 @@ from bs4 import BeautifulSoup
 def request_article_titles(url):
     request = requests.get(url)
     request_html = request.text
-    article_finder = BeautifulSoup(request_html)
-    title = article_finder.find('span', 'articletitle')
-    return title
+    article_finder = BeautifulSoup(request_html, 'lxml')
+    title = article_finder.findAll("h2")
+
+    for el in title:
+        try:
+            print(el.text.strip())
+        except "error":
+            pass
 
 
 def main():

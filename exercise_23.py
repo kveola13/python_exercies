2388efac7e9576964183f4628d0682f4e92fb9c5
olakv <kveola13@student.westerdals.no>
2020-03-14 18:32:12 +0100

    started exercise_23

---
+++ exercise_23.py
@@ -0,0 +1,16 @@
+def read_from_file_to_lists(first_file_name, second_file_name):
+    with open(first_file_name + '.txt', 'r') as file_reader:
+        first_file_text = file_reader.read()
+    pass
+
+
+def find_overlapping_numbers_in_lists():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

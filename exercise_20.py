23c48f237ada84f2f4d3c93aa4d9e94da44eeac1
olakv <kveola13@student.westerdals.no>
2020-03-14 10:12:11 +0100

    completed exercise_20

--- exercise_20.py
+++ exercise_20.py
@@ -1,36 +1,31 @@
-import math
-
-
 def binary_tree(ordered_list, number_to_search):
-    ordered_list.sort()
     list_copy = ordered_list
-    low = list_copy[0]
-    high = list_copy[len(list_copy) - 1]
-    mid = round((len(list_copy)) / 2)
-    if len(list_copy) == 1:
-        return "Could not find number in list"
-    else:
-        while low <= high:
-            print(len(list_copy))
-            print(mid)
-            if list_copy[mid] == number_to_search:
-                return "Found at index: " + str(mid)
-            elif len(list_copy) == 1:
-                return "Could not find number in list"
-            elif number_to_search < list_copy[mid]:
-                list_copy = list_copy[:mid:]
-                print(list_copy)
-                binary_tree(list_copy, number_to_search)
-            elif number_to_search > list_copy[mid]:
-                list_copy = list_copy[mid::]
-                print(list_copy)
-                binary_tree(list_copy, number_to_search)
+    while True:
+        mid = int(len(list_copy) / 2)
+        if number_to_search == list_copy[mid]:
+            return True, "Found at index: " + str(mid)
+        elif number_to_search < list_copy[mid]:
+            list_copy = list_copy[:mid]
+        elif number_to_search > list_copy[mid]:
+            list_copy = list_copy[mid:]
+        if len(list_copy) == 1:
+            if number_to_search == list_copy[0]:
+                return True, "Found at index: 0"
+            return False, "Could not find number in list"
 
 
 def main():
-    ordered_list = [1, 3, 5, 30, 42, 43, 500]
-    print(binary_tree(ordered_list, 45))
-    # print(binary_tree(ordered_list, 9))
+    ordered_list = [1, 3, 5, 30, 31, 42, 43, 500]
+    print(binary_tree(ordered_list, 31)[1])
+    print(binary_tree(ordered_list, 1)[1])
+    print(binary_tree(ordered_list, 3)[1])
+    print(binary_tree(ordered_list, 43)[1])
+    print(binary_tree(ordered_list, 500)[1])
+    print(binary_tree(ordered_list, 32)[1])
+    print(binary_tree(ordered_list, 2)[1])
+    print(binary_tree(ordered_list, 4)[1])
+    print(binary_tree(ordered_list, 6)[1])
+    print(binary_tree(ordered_list, 44)[1])
 
 
 if __name__ == '__main__':

bd79dad7c5d4a93cb0e0bd1506446da1d98fd526
olakv <kveola13@student.westerdals.no>
2020-03-14 20:41:13 +0100

    completed exercise_23

--- exercise_23.py
+++ exercise_23.py
@@ -1,15 +1,27 @@
 def read_from_file_to_lists(first_file_name, second_file_name):
+    first_list = []
+    second_list = []
     with open(first_file_name + '.txt', 'r') as file_reader:
-        first_file_text = file_reader.read()
-    pass
+        while file_reader.readline() != "":
+            first_list.append(file_reader.readline().strip('\n').strip(" "))
+    with open(second_file_name + '.txt', 'r') as file_reader:
+        while file_reader.readline() != "":
+            second_list.append(file_reader.readline().strip('\n').strip(" "))
+    return first_list, second_list
 
 
 def find_overlapping_numbers_in_lists():
-    pass
+    first_file_name = "text1"
+    second_file_name = "text2"
+    common_list = []
+    for el in read_from_file_to_lists(first_file_name, second_file_name)[0]:
+        if el in read_from_file_to_lists(first_file_name, second_file_name)[1]:
+            common_list.append(el)
+    return common_list
 
 
 def main():
-    pass
+    print(find_overlapping_numbers_in_lists())
 
 
 if __name__ == '__main__':

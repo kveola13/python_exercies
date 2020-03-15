346d327a5e847ba6821dbc8303c86364062d3920
olakv <kveola13@student.westerdals.no>
2020-03-15 20:14:42 +0100

    populating a 3x3 board dynamically, almost done with exercise_26

--- exercise_26.py
+++ exercise_26.py
@@ -4,17 +4,27 @@ def tic_tac_toe(row, column):
 
 
 def check_game_is_won(game_board):
-    count = 0
-    for num in range(len(game_board)):
-        for second_num in range(len(game_board[num])):
-            game_board[num][second_num] = count
-            count += 1
-    return game_board
+    for num in range(0, 3):
+        row = {game_board[num][0], game_board[num][1], game_board[num][2]}
+        if len(row) == 1 and game_board[num][0] != 0:
+            return game_board[num][0]
+    for num in range(0, 3):
+        column = [game_board[0][num], game_board[1][num], game_board[2][num]]
+        if len(column) == 1 and game_board[0][num] != 0:
+            return game_board[0][num]
+    first_across = {game_board[0][0], game_board[1][1], game_board[2][2]}
+    second_across = {game_board[0][2], game_board[1][1], game_board[2][0]}
+    if len(first_across) == 1 or len(second_across) == 1 and game_board[1][1] != 0:
+        return game_board[1][1]
+    return 0
 
 
 def main():
-    # print(tic_tac_toe(3, 3))
+    game_board = [[2, 2, 1],
+                  [2, 1, 2],
+                  [1, 1, 1]]
     print(check_game_is_won(tic_tac_toe(3, 3)))
+    print(check_game_is_won(game_board))
 
 
 if __name__ == '__main__':

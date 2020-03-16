b4727caa6602b10c3252101146cba43fab7f920a
olakv <kveola13@student.westerdals.no>
2020-03-16 17:09:01 +0100

    adjustments to be more dynamic

--- exercise_26.py
+++ exercise_26.py
@@ -9,19 +9,19 @@ def check_game_is_won(game_board):
         if len(row) == 1 and game_board[num][0] != 0:
             if game_board[num][0] == 0:
                 return True, "Draw!"
-            return True, "Player " + str(game_board[num][0]) + " wins!"
+            return False, "Player " + str(game_board[num][0]) + " wins!"
     for num in range(0, len(game_board)):
         column = [game_board[0][num], game_board[1][num], game_board[2][num]]
         if len(column) == 1 and game_board[0][num] != 0:
             if game_board[num][0] == 0:
                 return True, "Draw!"
-            return True, "Player " + str(game_board[num][0]) + " wins!"
+            return False, "Player " + str(game_board[num][0]) + " wins!"
     first_across = {game_board[0][0], game_board[1][1], game_board[2][2]}
     second_across = {game_board[0][2], game_board[1][1], game_board[2][0]}
     if len(first_across) == 1 or len(second_across) == 1 and game_board[1][1] != 0:
         if game_board[1][1] == 0:
             return True, "Draw!"
-        return True, "Player " + str(game_board[1][1]) + " wins!"
+        return False, "Player " + str(game_board[1][1]) + " wins!"
     return False, "Keep playing"
 
 
@@ -29,28 +29,35 @@ def tic_tac_toe(game_board):
     game_board_copy = game_board
     player_x_turn = True
     player_coords = ""
-    while check_game_is_won(game_board_copy):
+    while check_game_is_won(game_board_copy) or check_game_is_won(game_board_copy) != 'Player 1 wins!' or \
+            check_game_is_won(game_board_copy) != 'Player 2 wins!':
+        print(check_game_is_won(game_board_copy))
         try:
             player_coords = tuple(
                 map(int,
                     input("Please choose coordinates, separated with a comma:\nExit by typing 4,4\n").strip().split(
                         ',')))
-        except ValueError or IndexError or TypeError:
+        except ValueError:
             print("Invalid coordinates, try again")
             tic_tac_toe(game_board_copy)
-        if player_coords == (4, 4):
-            break
-        player_coords = (player_coords[0] - 1, player_coords[1] - 1)
-        if player_x_turn and game_board_copy[player_coords[0]][player_coords[1]] == 0:
-            game_board_copy[player_coords[0]][player_coords[1]] = 1
-            player_x_turn = not player_x_turn
-            print(player_x_turn)
-            print(game_board_copy)
-        elif not player_x_turn and game_board_copy[player_coords[0]][player_coords[1]] == 0:
-            game_board_copy[player_coords[0]][player_coords[1]] = 2
-            player_x_turn = not player_x_turn
-            print(player_x_turn)
-            print(game_board_copy)
+        except IndexError:
+            print("Invalid coordinates, try again")
+            tic_tac_toe(game_board_copy)
+        except TypeError:
+            print("Invalid coordinates, try again")
+            tic_tac_toe(game_board_copy)
+        finally:
+            if player_coords == (4, 4):
+                break
+            player_coords = (player_coords[0] - 1, player_coords[1] - 1)
+            if player_x_turn and game_board_copy[player_coords[0]][player_coords[1]] == 0:
+                game_board_copy[player_coords[0]][player_coords[1]] = 1
+                player_x_turn = not player_x_turn
+                print(game_board_copy)
+            elif not player_x_turn and game_board_copy[player_coords[0]][player_coords[1]] == 0:
+                game_board_copy[player_coords[0]][player_coords[1]] = 2
+                player_x_turn = not player_x_turn
+                print(game_board_copy)
     return game_board_copy
 
 

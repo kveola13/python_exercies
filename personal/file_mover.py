8c7be3da3c6906f95197d568acdaa6aaa27d477b
olakv <kveola13@student.westerdals.no>
2020-03-29 00:07:34 +0100

    teaching folder manipulation over discord

--- personal/file_mover.py
+++ personal/file_mover.py
@@ -1,5 +1,27 @@
+import os
+import random
+import shutil
+
+
+def remove_folders(folder):
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+
+
+def create_folder(folder):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+        os.chdir(folder)
+        print(os.getcwd())
+        rand_list = random.sample(range(97, 123), 26)
+        for el in rand_list:
+            os.mkdir(chr(el))
+
+
 def main():
-    pass
+    folder_name = "testfolder"
+    create_folder(folder_name)
+    remove_folders(folder_name)
 
 
 if __name__ == '__main__':
